4c9f3db57ad93f33a5ce3407b284eb24d33cc4c3
meiring <meiring@checkoutjoy.com>
2025-02-07 13:41:26 +0200

    feat: added charge_autior

--- transaction_test.go
+++ transaction_test.go
@@ -1,6 +1,7 @@
 package paystack
 
 import (
+	"context"
 	"fmt"
 	"math/rand"
 	"testing"
@@ -17,9 +18,28 @@ func TestInitializeTransaction(t *testing.T) {
 		client: getClient("sk_test_e39ce23869e6e677121a5e6ef691a8c3d835f0bb"),
 	}
 
-	_, err := transaction.Initialize(txn)
+	_, err := transaction.Initialize(context.Background(), txn)
 	if err != nil {
 		t.Error(err)
 	}
 
+}
+
+func TestChargeAuthorization(t *testing.T) {
+	txn := ChargeAuthorizationRequest{
+		Email:             "koos@test.com",
+		Amount:            6000,
+		AuthorizationCode: "AUTH_468hvddfw6",
+		Currency:          "ZAR",
+		Reference:         fmt.Sprintf("ref_%d", rand.Intn(1000000)),
+	}
+
+	transaction := TransactionService{
+		client: getClient("sk_test_e39ce23869e6e677121a5e6ef691a8c3d835f0bb"),
+	}
+
+	_, err := transaction.ChargeAuthorization(context.Background(), txn)
+	if err != nil {
+		t.Error(err)
+	}
 }
